b73f77bfd4f2440be7271ab5ff118b2d2011bff9
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-03-12 21:46:18 -0300

    adicionado main para fazer gerenciamento de routas

---
+++ src/main/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Routes from '../route/routes'
+
+import Navbar from '../template/navbar/navbar'
+
+export default props => (
+  <div>
+    <Navbar />
+    <Routes />
+  </div>
+)
