083f9b6ec65e2eec941f9fece1876d49ad280b1a
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-03-12 21:45:55 -0300

    adicionado página de consulta

---
+++ src/consultas/consultas.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Consultas extends Component {
+  render () {
+    return (
+      <div className='row consultas'>
+        Relatorio de Consultas
+      </div>
+    )
+  }
+}
+
+export default Consultas
